10bb6903f8a797570208dae59cd752512bdd1317
pmuren <paulmuren@gmail.com>
2013-10-08 18:36:00 -0400

    Added tiebreakers to the PokerHand class's compareTo method. Now the list that PokerHandEvaluator generates is truly in order from worst to best PokerHand!

--- src/com/forrestpruitt/texas/PokerHand.java
+++ src/com/forrestpruitt/texas/PokerHand.java
@@ -90,11 +90,6 @@ public class PokerHand implements Comparable
 		return ranks.size();
 	}
 
-	public HandType getHandType()
-	{
-		return handType;
-	}
-
 	public int getNumUniqueSuits()
 	{
 		ArrayList<Suit> suits = new ArrayList<Suit>();
@@ -130,25 +125,341 @@ public class PokerHand implements Comparable
 	{
 		return cards.get(0);
 	}
+	
+	public HandType getHandType()
+	{
+		return handType;
+	}
 
 	public ArrayList<Card> getCards()
 	{
 		return cards;
 	}
 
-	public int compareTo(PokerHand otherPokerHand)
+	public int breakTieHighCard(PokerHand otherPokerHand)
 	{
-		int thisPokerHandRank  = this.getHandType().ordinal();
-		int otherPokerHandRank = otherPokerHand.getHandType().ordinal();
-		return thisPokerHandRank - otherPokerHandRank;
+		int index = 4;
+		Card thisCard = this.getCards().get(index);
+		Card otherCard = otherPokerHand.getCards().get(index);
+		while(thisCard.getRank() == otherCard.getRank() && index >= 0)
+		{
+			thisCard = this.getCards().get(index);
+			otherCard = otherPokerHand.getCards().get(index);
+			index--;
+		}
+		return thisCard.getRank().ordinal() - otherCard.getRank().ordinal();
 	}
 
+	//Necessary for implementing 'Comparable' so that PokerHands can be sorted by Collections.sort
+	//The PokerHandEvaluator sorts PokerHand objects to find the best (greatest handType) one
 	public int compareTo(Object otherObject)
 	{
 		PokerHand otherPokerHand = (PokerHand)otherObject;
 		int thisPokerHandRank  = this.getHandType().ordinal();
 		int otherPokerHandRank = otherPokerHand.getHandType().ordinal();
-		return thisPokerHandRank - otherPokerHandRank;
+		//If the handTypes of the hands are not the same
+		if(thisPokerHandRank != otherPokerHandRank)
+		{
+			return thisPokerHandRank - otherPokerHandRank;
+		}
+		else //Tiebreakers
+		{
+			if(this.getHandType() == handType.HIGHCARD)
+			{
+				return breakTieHighCard(otherPokerHand);	
+			}
+
+			if(this.getHandType() == handType.ONEPAIR)
+			{
+				boolean found;
+				int index;
+				Rank thisPairRank = Rank.TWO;
+				Rank otherPairRank = Rank.TWO;
+				
+				index = 4;
+				found = false;
+				//Find the rank of this PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(this.getCards().get(index).getRank()) == 2)
+					{
+						thisPairRank = this.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				index = 4;
+				found = false;
+				//Find the rank of the other PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(otherPokerHand.getCards().get(index).getRank()) == 2)
+					{
+						otherPairRank = otherPokerHand.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				//If the pairs are different ranks, return the winner
+				if(thisPairRank.ordinal() != otherPairRank.ordinal())
+				{
+					return thisPairRank.ordinal() - otherPairRank.ordinal();
+				}
+				else //use high card to determine winner
+				{
+					return breakTieHighCard(otherPokerHand);
+				}
+			}
+
+			if(this.getHandType() == handType.TWOPAIR)
+			{
+				boolean found;
+				int index;
+				Rank thisHighPairRank = Rank.TWO;
+				Rank thisLowPairRank = Rank.TWO;
+				Rank otherHighPairRank = Rank.TWO;
+				Rank otherLowPairRank = Rank.TWO;
+				
+				index = 4;
+				found = false;
+				//Find the rank of this hand's first pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(this.getCards().get(index).getRank()) == 2)
+					{
+						thisHighPairRank = this.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				index = 4;
+				found = false;
+				//Find the rank of this hand's second pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(this.getCards().get(index).getRank()) == 2
+					   && this.getCards().get(index).getRank() != thisHighPairRank)
+					{
+						if(this.getCards().get(index).getRank().ordinal() > thisHighPairRank.ordinal())
+						{
+							thisLowPairRank = thisHighPairRank;
+							thisHighPairRank = this.getCards().get(index).getRank();
+						}
+						else
+						{
+							thisLowPairRank = this.getCards().get(index).getRank();
+						}
+						found = true;
+					}
+					index--;
+				}
+
+				index = 4;
+				found = false;
+				//Find the rank of the other hand's first pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(otherPokerHand.getCards().get(index).getRank()) == 2)
+					{
+						otherHighPairRank = otherPokerHand.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				index = 4;
+				found = false;
+				//Find the rank of the other hand's second pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(otherPokerHand.getCards().get(index).getRank()) == 2
+					   && otherPokerHand.getCards().get(index).getRank() != otherHighPairRank)
+					{
+						if(otherPokerHand.getCards().get(index).getRank().ordinal() > otherHighPairRank.ordinal())
+						{
+							otherLowPairRank = otherHighPairRank;
+							otherHighPairRank = otherPokerHand.getCards().get(index).getRank();
+						}
+						else
+						{
+							otherLowPairRank = otherPokerHand.getCards().get(index).getRank();
+						}
+						found = true;
+					}
+					index--;
+				}
+
+				//Compare high pairs' ranks and return winner
+				if(thisHighPairRank.ordinal() != otherHighPairRank.ordinal())
+				{
+					return thisHighPairRank.ordinal() - otherHighPairRank.ordinal();
+				}
+				//If high pairs' ranks are the same, compare low pairs' ranks and return winner
+				else if(thisLowPairRank.ordinal() != otherLowPairRank.ordinal())
+				{
+					return thisLowPairRank.ordinal() - otherLowPairRank.ordinal();
+				}
+				//If both high and low pairs are the same ranks, use high card
+				else
+				{
+					return breakTieHighCard(otherPokerHand);
+				}
+
+			}
+
+			if(this.getHandType() == handType.THREEOFAKIND)
+			{
+				boolean found;
+				int index;
+				Rank thisTrioRank = Rank.TWO;
+				Rank otherTrioRank = Rank.TWO;
+				
+				index = 4;
+				found = false;
+				//Find the rank of this PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(this.getCards().get(index).getRank()) == 3)
+					{
+						thisTrioRank = this.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				index = 4;
+				found = false;
+				//Find the rank of the other PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(otherPokerHand.getCards().get(index).getRank()) == 3)
+					{
+						otherTrioRank = otherPokerHand.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				//If the pairs are different ranks, return the winner
+				if(thisTrioRank.ordinal() != otherTrioRank.ordinal())
+				{
+					return thisTrioRank.ordinal() - otherTrioRank.ordinal();
+				}
+				else //use high card to determine winner
+				{
+					return breakTieHighCard(otherPokerHand);
+				}	
+			}
+
+			if(this.getHandType() == handType.STRAIGHT)
+			{
+				return breakTieHighCard(otherPokerHand);
+			}
+
+			if(this.getHandType() == handType.FLUSH)
+			{
+				return breakTieHighCard(otherPokerHand);
+			}
+
+			if(this.getHandType() == handType.FULLHOUSE)
+			{
+				boolean found;
+				int index;
+				Rank thisTrioRank = Rank.TWO;
+				Rank otherTrioRank = Rank.TWO;
+				
+				index = 4;
+				found = false;
+				//Find the rank of this PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(this.getCards().get(index).getRank()) == 3)
+					{
+						thisTrioRank = this.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				index = 4;
+				found = false;
+				//Find the rank of the other PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(otherPokerHand.getCards().get(index).getRank()) == 3)
+					{
+						otherTrioRank = otherPokerHand.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				//If the pairs are different ranks, return the winner
+				if(thisTrioRank.ordinal() != otherTrioRank.ordinal())
+				{
+					return thisTrioRank.ordinal() - otherTrioRank.ordinal();
+				}
+				else //use high card to determine winner
+				{
+					return breakTieHighCard(otherPokerHand);
+				}	
+			}
+
+			if(this.getHandType() == handType.FOUROFAKIND)
+			{
+				boolean found;
+				int index;
+				Rank thisQuartetRank = Rank.TWO;
+				Rank otherQuartetRank = Rank.TWO;
+				
+				index = 4;
+				found = false;
+				//Find the rank of this PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(this.getCards().get(index).getRank()) == 4)
+					{
+						thisQuartetRank = this.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				index = 4;
+				found = false;
+				//Find the rank of the other PokerHand's pair
+				while(!found)
+				{
+					if(getNumOccurencesOfRank(otherPokerHand.getCards().get(index).getRank()) == 4)
+					{
+						otherQuartetRank = otherPokerHand.getCards().get(index).getRank();
+						found = true;
+					}
+					index--;
+				}
+
+				//If the pairs are different ranks, return the winner
+				if(thisQuartetRank.ordinal() != otherQuartetRank.ordinal())
+				{
+					return thisQuartetRank.ordinal() - otherQuartetRank.ordinal();
+				}
+				else //use high card to determine winner
+				{
+					return breakTieHighCard(otherPokerHand);
+				}	
+
+			}
+
+			if(this.getHandType() == handType.STRAIGHTFLUSH)
+			{
+				return breakTieHighCard(otherPokerHand);
+			}
+
+			return 0;
+		}
 	}
 
 	public String toString()
